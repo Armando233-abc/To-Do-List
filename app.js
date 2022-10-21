f339280cb93e34a94cfbe5fc54bbfa08125fdf85
Armando233-abc <armandovigliotti5@gmail.com>
2022-10-21 15:42:00 +0200

    simple css style

--- app.js
+++ app.js
@@ -6,16 +6,22 @@ function aggiungi() {
 
 	const to_do_list = document.querySelector("#to_do_list")
 
+	const new_div = document.createElement("div")
+	new_div.setAttribute("class", "item_container")
+	new_div.setAttribute("id", `div${contatore}`);
+
 	const new_li = document.createElement("li");
-	new_li.setAttribute("id", `li${contatore}`)
 	new_li.innerHTML = testo
 
 	const button = document.createElement("button");
-	button.setAttribute("onclick", `elimina(li${contatore})`)
+	button.setAttribute("onclick", `elimina(div${contatore})`)
+	button.setAttribute("id", "elimina_button")
 	button.innerHTML = "X"
 
-	new_li.appendChild(button)
-	to_do_list.appendChild(new_li);
+	new_div.appendChild(new_li);
+	new_div.appendChild(button)
+
+	to_do_list.appendChild(new_div)
 
 	input.value = ""
 	contatore += 1
